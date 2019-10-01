1aed6cf419105153db169569104b19ac286f4453
Bernarditacha <39970407+Bernarditacha@users.noreply.github.com>
2019-10-01 17:46:24 -0300

    Agrego cambios a la primera parte del trabajo practico

--- tp3/EntregaFinalPrimeraParte/js/archivo2.js
+++ tp3/EntregaFinalPrimeraParte/js/archivo2.js
@@ -1,21 +1,14 @@
-/*function reloj(){
-    let horaActual  = new Date();
-    let horas = horaActual.getHours();
-    let minutos = horaActual.getMinutes();
-    let segundos = horaActual.getSeconds();
-    horas = (horas > 12)? horas -12 : horas;
-    horas = (horas == '00')? 12 : horas;
-
-    let segundero = document.querySelector(".box6");
-    let minutero = document.querySelector(".box7");
-    let horero = document.querySelector(".box8");
-
-    let rotateS = "rotate("+segundos*6 + "deg)";
-    let rotateM = "rotate("+minutos *6 + "deg)";
-    let rotateH = "rotate("+((horas*30) + (minutos / 2))+"deg)";
-
-    segundero.style.transform = rotateS;
-    minutero.style.transform = rotateM;
-    horero.style.transform = rotateH;
-}
-setInterval(reloj,1000);*/
+// --- EJERICIO 3 --- //
+let box = document.querySelector(".box10");
+window.onload = function() {
+  box.addEventListener('mousemove', function(event){
+    var imagen = document.querySelector(".imagen2");
+      var  eX = event.clientX;
+      var eY = event.clientY;
+      eX = eX - 300;
+      eY = eY - 300;
+      imagen.style.left = eX+"px";
+      imagen.style.top = eY+"px";
+      
+    });
+  }
